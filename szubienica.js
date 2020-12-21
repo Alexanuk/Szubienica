4dc8a252f17334cd1a98dbf18e2de86ffcbdb735
Alexander <Alexanuk@users.noreply.github.com>
2020-12-21 16:18:45 +0100

    Create szubienica.js

---
+++ szubienica.js
@@ -0,0 +1,259 @@
+
+
+var Fraza = new Array(105)
+
+	Fraza [0]="Albo rybki albo akwarium"
+	Fraza [1]="Apetyt rośnie w miarę jedzenia"
+	Fraza [2]="Bez pracy nie ma kołaczy"
+	Fraza [3]="Być w Rzymie i papieża nie widzieć"
+	Fraza [4]="Co było a nie jest nie pisze się w rejestr"
+	Fraza [5]="Co dwie  głowy to nie jedna"
+	Fraza [6]="Co nas nie zabije  to nas wzmocni"
+	Fraza [7]="Co się stało to się nie odstanie"
+	Fraza [8]="Szyte grubymi nićmi"
+	Fraza [9]="Nie jest to ostatnie słowo"
+	Fraza [10]="Chodzić  jak paw"
+	Fraza [11]="Chodzić  na rzęsach"
+	Fraza [12]="Ciągnąć kogoś za język"
+	Fraza [13]="Chwytać za słowa"
+	Fraza [14]="Chwytać za pióro"
+	Fraza [15]="Chytry szczwany lis"
+	Fraza [16]="Chylić czoła"
+	Fraza [17]="Czarny koń"
+	Fraza [18]="Czuć się jak ryba w wodzie"
+	Fraza [19]="Darowanemu koniowi nie patrzy się w zęby"
+	Fraza [20]="Domek z kart"
+	Fraza [21]="Do wesela się zagoi"
+	Fraza [22]="Działać jak czerwona płachta na byka"
+	Fraza [23]="Dzielić  skórę na niedźwiedziu"
+	Fraza [24]="Elegancki świat"
+	Fraza [24]="Elektryka prąd nie tyka"
+	Fraza [26]="Figa z makiem"
+	Fraza [27]="Fortuna często rozumowi przeczy"
+	Fraza [28]="Fortuna kołem się toczy"
+	Fraza [29]="Francuski piesek"
+	Fraza [30]="Gadał dziad  do obrazu a obraz do niego  ani razu"
+	Fraza [31]="Gdyby kózka nie skakała toby nóżki  nie złamała"
+	Fraza [32]="Głową  muru nie przebijesz"
+	Fraza [33]="Grać komuś na nerwach"
+	Fraza [34]="Hołdować czemuś"
+	Fraza [35]="Hulaj dusza piekła nie ma"
+	Fraza [36]="Hultajska praca wniwecz się obraca"
+	Fraza [37]="I w Paryżu  nie zrobią  z owsa ryżu"
+	Fraza [38]="I wilk  syty i owca  cała"
+	Fraza [39]="Im kot starszy tym ogon twardszy"
+	Fraza [40]="Indyk myślał  o niedzieli a w sobotę  łeb ucieli"
+	Fraza [41]="Po linii najmniejszego oporu"
+	Fraza [42]="Jedna jaskółka wiosny  nie czyni"
+	Fraza [43]="Jeśli wejdziesz między wrony musisz krakać tak jak one"
+	Fraza [44]="Kamień na kamieniu nie został"
+	Fraza [45]="Kto nie ma w głowie ten ma w nogach"
+	Fraza [46]="Kto pod kim  dołki  kopie ten sam w nie wpada"
+	Fraza [47]="Kto pyta nie błądzi"
+	Fraza [48]="Kaszka z mlekiem"
+	Fraza [49]="Korona z głowy nie spada komuś"
+	Fraza [50]="Krakowskim targiem"
+	Fraza [51]="Ma głowę na karku"
+	Fraza [52]="Nie dać sobie w kaszę dmuchać"
+	Fraza [53]="Lepiej późno  niż wcale"
+	Fraza [54]="Lepiej zapobiegać niż leczyć"
+	Fraza [55]="Lepsze jest wrogiem dobrego"
+	Fraza [56]="Lepszy rydz niż nic"
+	Fraza [57]="Lepszy w domu groch kapusta niż na wojnie kura tłusta"
+	Fraza [58]="Lepszy wróbel w garści niż gołąb  na dachu"
+	Fraza [59]="Łaska pańska na pstrym koniu  jeździ"
+	Fraza [60]="Mądry przyjmie radę głupi  nią wzgardzi"
+	Fraza [61]="Mądrzejsze jajko od kury"
+	Fraza [62]="Miłość jest ślepa"
+	Fraza [63]="Miłość miłość  rodzi"
+	Fraza [64]="Mowa jest srebrem a milczenie złotem"
+	Fraza [65]="Na układy nie ma rady"
+	Fraza [66]="Na wołowej skórze by tego nie spisał"
+	Fraza [67]="Najstarsi  górale  nie pamiętają"
+	Fraza [68]="Nauka nie poszła w las"
+	Fraza [69]="Nie czas żałować róż gdy płoną  lasy"
+	Fraza [70]="Nie ma gdzie szpilki wcisnąć"
+	Fraza [71]="Nie ma reguły bez wyjątku"
+	Fraza [72]="Nie ma róży bez kolców"
+	Fraza [72]="Nie taki diabeł straszny jak go malują"
+	Fraza [74]="Od rzemyczka do koniczka"
+	Fraza [75]="Od swej jabłoni blisko jabłko pada"
+	Fraza [76]="Okazja czyni złodzieja"
+	Fraza [77]="Oko za oko ząb za ząb"
+	Fraza [78]="Pieczone gołąbki same nie lecą do gąbki"
+	Fraza [79]="Powieka nawet nie drgnęła komuś"
+	Fraza [80]="Póty dzban wodę nosi póki się ucho nie urwie"
+	Fraza [81]="Raz na wozie  raz pod wozem"
+	Fraza [82]="Rozbićw proch"
+	Fraza [83]="Rozchodzić się po kościach"
+	Fraza [84]="Sam nie zje i drugiemu nie da"
+	Fraza [85]="Słuchać  uchem a nie brzuchem"
+	Fraza [86]="Szukać  igły w stogu siana"
+	Fraza [87]="Tak krawiec kraje jak mu materii staje"
+	Fraza [88]="Ten się śmieje kto się śmieje  ostatni"
+	Fraza [89]="Trafiła kosa na kamień"
+	Fraza [90]="Trafiło się ślepej kurze ziarno"
+	Fraza [91]="Twardy orzech do zgryzienia"
+	Fraza [92]="Uderz w stół a nożyce się odezwą"
+	Fraza [93]="Upiec dwie pieczenie na jednym ogniu"
+	Fraza [94]="W przyrodzie nic nie ginie"
+	Fraza [95]="Włos  komuś z głowy nie spadnie"
+	Fraza [96]="Wyłożyć kawę na ławę"
+	Fraza [97]="Wypływać na szerokie wody"
+	Fraza [98]="Z tej mąki chleba nie będzie"
+	Fraza [99]="Znać jak własną kieszeń"
+	Fraza [100]="Zwiesić nos na kwintę"
+	Fraza [101]="Żyć nie umierać"
+	Fraza [102]="Żyć jak pies z kotem"
+	Fraza [103]="Żywe srebro"
+	
+	function getMessage() 
+	
+{
+return Fraza[Math.floor(Math.random() * Fraza.length)];
+}
+
+var haslo =getMessage();
+haslo = haslo.toUpperCase();
+
+
+
+var dlugosc = haslo.length; 
+var ilosc_skuch = 0;
+
+var yes = new Audio ("yes.wav")
+var no = new Audio ("no.wav")
+
+var haslo1 = "";
+
+for(i=0; i<dlugosc; i++)
+{
+	if(haslo.charAt(i)==" ") haslo1 = haslo1+" ";
+	else haslo1 = haslo1 + "-";
+}
+
+function wypisz_haslo()
+{
+	document.getElementById("plansza").innerHTML = haslo1;
+}
+
+window.onload = start;
+
+var litery = new Array(35);
+
+litery [0] = "A"; 
+litery [1] = "Ą"; 
+litery [2] = "B"; 
+litery [3] = "C"; 
+litery [4] = "Ć"; 
+litery [5] = "D"; 
+litery [6] = "E"; 
+litery [7] = "Ę"; 
+litery [8] = "F"; 
+litery [9] = "G"; 
+litery [10] = "H"; 
+litery [11] = "I"; 
+litery [12] = "J"; 
+litery [13] = "K"; 
+litery [14] = "L"; 
+litery [15] = "Ł"; 
+litery [16] = "M"; 
+litery [17] = "N"; 
+litery [18] = "Ń"; 
+litery [19] = "O"; 
+litery [20] = "Ó"; 
+litery [21] = "P"; 
+litery [22] = "Q"; 
+litery [23] = "R"; 
+litery [24] = "S"; 
+litery [25] = "Ś"; 
+litery [26] = "T"; 
+litery [27] = "U"; 
+litery [28] = "V"; 
+litery [29] = "W"; 
+litery [30] = "X"; 
+litery [31] = "Y"; 
+litery [32] = "Z"; 
+litery [33] = "Ż"; 
+litery [34] = "Ź"; 
+
+function start ()
+{
+	var tresc_diva="";
+	
+	for (i=0; i<=34; i++)
+		{
+			var element = "lit"+i;
+			tresc_diva = tresc_diva + '<div class ="litera" onclick ="sprawdz('+i+')" id="'+element+'">'+litery[i]+'</div>';
+			if ((i+1) % 7 == 0 ) tresc_diva = tresc_diva + '<div style="clear:both;"></div>';
+		}
+	
+	document.getElementById("alfabet").innerHTML= tresc_diva;
+	
+	wypisz_haslo();
+}
+	
+String.prototype.ustawZnak = function(miejsce, znak)
+{
+	if(miejsce>this.length-1) return this.toString();
+	else return this.substr(0, miejsce)+znak +this.substr(miejsce+1);
+}
+	
+	
+function sprawdz(nr)
+{
+	
+	var trafiona = false;
+	
+	for(i=0; i<dlugosc; i++)
+	{
+		if (haslo.charAt(i)==litery[nr])
+		{
+			haslo1 = haslo1.ustawZnak(i,litery[nr]);
+			trafiona = true;
+		}
+	}
+	
+	if (trafiona==true)
+	{
+		yes.play();
+		var element = "lit"+nr;
+		document.getElementById(element).style.background = "#003300";
+		document.getElementById(element).style.color = "#00C000";
+		document.getElementById(element).style.border = "3px solid #00C000";
+		document.getElementById(element).style.cursor = "default";
+				
+		wypisz_haslo();
+		
+	}
+	else
+	{
+		no.play ();
+		var element = "lit"+nr;
+		document.getElementById(element).style.background = "#330000";
+		document.getElementById(element).style.color = "#C00000";
+		document.getElementById(element).style.border = "3px solid #C00000";
+		document.getElementById(element).style.cursor = "default";	
+		
+		document.getElementById(element).setAttribute("onclick", ";");
+		//skucha
+		ilosc_skuch++;
+		
+		var obraz = "img/s"+ilosc_skuch +".jpg";
+		document.getElementById("szubienica").innerHTML='<img src= " '+obraz+' " alt =""/>';
+		
+	}
+	
+	//wygrana
+	if (haslo == haslo1)
+	document.getElementById("alfabet").innerHTML = "Tak, to jest właśnie szukane przysłowie: <br />"+haslo+'<br /><br /><span class ="reset" onclick="location.reload()">JESZCZE RAZ ?</span>';
+	
+	//przegrana
+	if(ilosc_skuch>=9)
+	document.getElementById("alfabet").innerHTML = "Niestety, oblałeś totalnie ten test. Prawidłowe hasło to: <br/> "+haslo+'<br /><br /><span class ="reset" onclick="location.reload()">JESZCZE RAZ ?</span>';
+	
+	
+}
+		
+		
+	
